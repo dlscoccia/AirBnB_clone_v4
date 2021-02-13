646d7f4fed4f016782fbe5fd98e9a77ff98a22fb
dlscoccia <2054@holbertonschool.com>
2021-02-13 16:16:58 +0000

    Adding Script for 1-hbnb route, that checks amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,28 @@
+function docReady (fn) {
+    // see if DOM is already available
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+      // call on next available tick
+      setTimeout(fn, 1);
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+  
+  const amenityList = [];
+  docReady(function () {
+    const myDiv = $('.amenities input');
+    $(myDiv).click(function () {
+      const name = $(this).attr('data-name');
+      // const id = $(this).attr('data-id');
+      if ($(this).is(':checked')) {
+        amenityList.push(name);
+      } else {
+        const index = amenityList.indexOf(name);
+        if (index !== -1) {
+          amenityList.splice(index, 1);
+        }
+      }
+      $('.amenities h4').text(amenityList.join(', '));
+    });
+  });
+  
