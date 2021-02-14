9d637e3228b88827bc9f726c0f5b4216777a61b2
xioperez01 <angie.xioperez@gmail.com>
2021-02-14 12:12:48 -0500

    New post in web_dynamic/static/scripts/4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -8,25 +8,44 @@ function docReady (fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
-
 const amenityList = [];
+const amenityListIds = [];
+const placesIds = [];
 docReady(function () {
   const myDiv = $('.amenities input');
   $(myDiv).click(function () {
     const name = $(this).attr('data-name');
+    const id = $(this).attr('data-id');
     // const id = $(this).attr('data-id');
     if ($(this).is(':checked')) {
       amenityList.push(name);
+      amenityListIds.push(id);
     } else {
       const index = amenityList.indexOf(name);
       if (index !== -1) {
         amenityList.splice(index, 1);
+        amenityListIds.splice(index, 1);
       }
     }
     $('.amenities h4').text(amenityList.join(', '));
   });
+  const myButton = $('button');
+  $(myButton).click(function () {
+    $('section.places article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({ amenities: amenityListIds }),
+      contentType: 'application/json',
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          placesIds.push(data[i].id);
+          $('section.places').append(`<article><div class="title_box"><h2>${data[i].name}</h2><div class="price_by_night">${data[i].price_by_night}</div></div><div class="information"><div class="max_guest">${data[i].max_guest}</div><div class="number_rooms">${data[i].number_rooms}</div><div class="number_bathrooms">${data[i].number_bathrooms}</div></div><div class="description">${data[i].description}</div></div></article>`);
+        }
+      }
+    });
+  });
 });
-
 $.ajax({
   type: 'GET',
   url: 'http://0.0.0.0:5001/api/v1/status/',
@@ -39,14 +58,14 @@ $.ajax({
     }
   }
 });
-
 $.ajax({
   type: 'POST',
   url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  data: JSON.stringify({}),
+  data: JSON.stringify({ amenities: [] }),
   contentType: 'application/json',
   success: function (data) {
     for (let i = 0; i < data.length; i++) {
+      placesIds.push(data[i].id);
       $('section.places').append(`<article><div class="title_box"><h2>${data[i].name}</h2><div class="price_by_night">${data[i].price_by_night}</div></div><div class="information"><div class="max_guest">${data[i].max_guest}</div><div class="number_rooms">${data[i].number_rooms}</div><div class="number_bathrooms">${data[i].number_bathrooms}</div></div><div class="description">${data[i].description}</div></div></article>`);
     }
   }
