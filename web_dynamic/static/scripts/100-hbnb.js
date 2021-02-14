a709e5896fdccfb457f56b08df8a3af4943d6954
xioperez01 <angie.xioperez@gmail.com>
2021-02-14 15:19:46 -0500

    Style web_dynamic/static/scripts/100-hbnb.js

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -16,86 +16,78 @@ const stateListIds = [];
 const cityListIds = [];
 const statesAndCities = [];
 docReady(function () {
-$('input').css('margin-right', '10px');
+  $('input').css('margin-right', '10px');
   const myAmenityCheckbox = $('.amenities input');
   const myStateCheckbox = $('.locations ul li h2 input');
   const myCityCheckbox = $('.locations ul li ul li input');
   $(myAmenityCheckbox).click(function () {
     const name = $(this).attr('data-name');
-const id = $(this).attr('data-id');
+    const id = $(this).attr('data-id');
     if ($(this).is(':checked')) {
-console.log(id);
       amenityList.push(name);
-amenityListIds.push(id);
+      amenityListIds.push(id);
     } else {
       const index = amenityList.indexOf(name);
       if (index !== -1) {
         amenityList.splice(index, 1);
-amenityListIds.splice(index, 1);
+        amenityListIds.splice(index, 1);
       }
     }
     $('.amenities h4').text(amenityList.join(', '));
- });
+  });
 
- $(myStateCheckbox).click(function () {
-const nameState = $(this).attr('data-name');
-const idState = $(this).attr('data-id');
+  $(myStateCheckbox).click(function () {
+    const nameState = $(this).attr('data-name');
+    const idState = $(this).attr('data-id');
     if ($(this).is(':checked')) {
-stateListIds.push(idState);
-statesAndCities.push(nameState);
+      stateListIds.push(idState);
+      statesAndCities.push(nameState);
     } else {
       const index = stateListIds.indexOf(idState);
-const indexStateCity = statesAndCities.indexOf(nameState);      
-if (index !== -1) {
+      const indexStateCity = statesAndCities.indexOf(nameState);
+      if (index !== -1) {
         stateListIds.splice(index, 1);
-} if (indexStateCity !== -1) {
-statesAndCities.splice(indexStateCity, 1);}      
-
+      } if (indexStateCity !== -1) {
+        statesAndCities.splice(indexStateCity, 1);
+      }
     }
-$('.locations h4').text(statesAndCities.join(', '));
-
- });
-
-$(myCityCheckbox).click(function () {
-const nameCity = $(this).attr('data-name');
-const idCity = $(this).attr('data-id');
+    $('.locations h4').text(statesAndCities.join(', '));
+  });
+  $(myCityCheckbox).click(function () {
+    const nameCity = $(this).attr('data-name');
+    const idCity = $(this).attr('data-id');
     if ($(this).is(':checked')) {
-cityListIds.push(idCity);
-statesAndCities.push(nameCity);
+      cityListIds.push(idCity);
+      statesAndCities.push(nameCity);
     } else {
       const index = cityListIds.indexOf(idCity);
-const indexStateCity = statesAndCities.indexOf(nameCity);
+      const indexStateCity = statesAndCities.indexOf(nameCity);
       if (index !== -1) {
         cityListIds.splice(index, 1);
- }
-if (indexStateCity !== -1) {
-statesAndCities.splice(indexStateCity, 1);}
-
+      }
+      if (indexStateCity !== -1) {
+        statesAndCities.splice(indexStateCity, 1);
+      }
     }
-$('.locations h4').text(statesAndCities.join(', '));
-
+    $('.locations h4').text(statesAndCities.join(', '));
+  });
+  const myButton = $('button');
+  $(myButton).click(function () {
+    $('section.places article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({ states: stateListIds, cities: cityListIds, amenities: amenityListIds }),
+      contentType: 'application/json',
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          placesIds.push(data[i].id);
+          $('section.places').append(`<article><div class="title_box"><h2>${data[i].name}</h2><div class="price_by_night">${data[i].price_by_night}</div></div><div class="information"><div class="max_guest">${data[i].max_guest}</div><div class="number_rooms">${data[i].number_rooms}</div><div class="number_bathrooms">${data[i].number_bathrooms}</div></div><div class="description">${data[i].description}</div></div></article>`);
+        }
+      }
+    });
+  });
 });
-
-
-const myButton = $('button');
-$(myButton).click(function () {
-$('section.places article').remove();
-$.ajax({
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-data: JSON.stringify({states: stateListIds, cities: cityListIds, amenities: amenityListIds}),  
-contentType: 'application/json',
-  success: function (data) {
-console.log(data);
-    for (let i = 0; i < data.length; i++) {
-placesIds.push(data[i].id);
-      $('section.places').append(`<article><div class="title_box"><h2>${data[i].name}</h2><div class="price_by_night">${data[i].price_by_night}</div></div><div class="information"><div class="max_guest">${data[i].max_guest}</div><div class="number_rooms">${data[i].number_rooms}</div><div class="number_bathrooms">${data[i].number_bathrooms}</div></div><div class="description">${data[i].description}</div></div></article>`);
-    }
-  }
-});
-});
-});
-
 $.ajax({
   type: 'GET',
   url: 'http://0.0.0.0:5001/api/v1/status/',
@@ -112,11 +104,11 @@ $.ajax({
 $.ajax({
   type: 'POST',
   url: 'http://0.0.0.0:5001/api/v1/places_search/',
-data: JSON.stringify({amenities: []}),  
-contentType: 'application/json',
+  data: JSON.stringify({ amenities: [] }),
+  contentType: 'application/json',
   success: function (data) {
     for (let i = 0; i < data.length; i++) {
-placesIds.push(data[i].id);
+      placesIds.push(data[i].id);
       $('section.places').append(`<article><div class="title_box"><h2>${data[i].name}</h2><div class="price_by_night">${data[i].price_by_night}</div></div><div class="information"><div class="max_guest">${data[i].max_guest}</div><div class="number_rooms">${data[i].number_rooms}</div><div class="number_bathrooms">${data[i].number_bathrooms}</div></div><div class="description">${data[i].description}</div></div></article>`);
     }
   }
